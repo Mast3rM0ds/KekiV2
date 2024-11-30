80ddfcc37a6805f768fb65838b36fcaf18040010
Mast3rM0ds <applab766@gmail.com>
2024-11-30 18:04:35 +0100

    Add project files.

---
+++ KekiEditor/GameProject/ProjectBrowser.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KekiEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowser.xaml
+    /// </summary>
+    public partial class ProjectBrowser : Window
+    {
+        public ProjectBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender == openKekiButton)
+            {
+                if(newKekiButton.IsChecked == true)
+                {
+                    newKekiButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+                openKekiButton.IsChecked = true;
+            }
+            else
+            {
+                if (openKekiButton.IsChecked == true)
+                {
+                    openKekiButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800,0,0,0);
+                }
+                newKekiButton.IsChecked = true;
+            }
+        }
+    }
+}
